2ef6d30ec150a8f76b485c481f758b9248720623
alxminyaev <alxminyaev@gmail.com>
2019-04-19 15:14:06 +0700

    FacilityLocalSource

--- data/src/main/kotlin/com/nstu/technician/data/datasource/local/FacilityLocalSource.kt
+++ data/src/main/kotlin/com/nstu/technician/data/datasource/local/FacilityLocalSource.kt
@@ -1,33 +1,35 @@
 package com.nstu.technician.data.datasource.local
 
+import com.nstu.technician.data.datasource.AddressDataSource
 import com.nstu.technician.data.datasource.FacilityDataSource
-import com.nstu.technician.data.datasource.local.dao.AddressDao
+import com.nstu.technician.data.datasource.LOCAL
 import com.nstu.technician.data.datasource.local.dao.FacilityDao
-import com.nstu.technician.data.datasource.local.dao.GpsDao
+import com.nstu.technician.data.datasource.local.dao.UtilDao
 import com.nstu.technician.data.dto.job.FacilityDTO
-import com.nstu.technician.data.until.*
+import com.nstu.technician.data.until.convertToFacilityDTO
+import com.nstu.technician.data.until.convertToFacilityEntity
 import javax.inject.Inject
+import javax.inject.Named
 
 class FacilityLocalSource @Inject constructor(
+    private val utilDao: UtilDao,
     private val facilityDao: FacilityDao,
-    private val addressDao: AddressDao,
-    private val gpsDao: GpsDao
+    @Named(LOCAL)
+    private val addressLocalSource: AddressDataSource
 ) : FacilityDataSource {
+
     override suspend fun findById(id: Long): FacilityDTO? {
         return facilityDao.findById(id)?.let { facilityEntity ->
-            gpsDao.findById(facilityEntity.addressId).let { gpsEntity ->
-                val addressDTO = addressDao.findById(facilityEntity.addressId)?.let { addressEntity ->
-                    gpsDao.findById(addressEntity.gpsPointId)?.convertToGpsPointDTO()?.let { GPSPointDTO ->
-                        addressEntity.convertToAddressDTO(GPSPointDTO)
-                    }
-                } ?: throw NullPointerException("address not found")
+            addressLocalSource.findById(facilityEntity.addressId)?.let { addressDTO ->
                 facilityEntity.convertToFacilityDTO(addressDTO)
             }
         }
     }
 
     override suspend fun save(facilityDTO: FacilityDTO) {
-        facilityDao.save(facilityDTO.convertToFacilityEntity())
-        gpsDao.save(facilityDTO.address.location.convertToGPSEntity())
+        utilDao.transaction {
+            addressLocalSource.save(facilityDTO.address)
+            facilityDao.save(facilityDTO.convertToFacilityEntity())
+        }
     }
 }
