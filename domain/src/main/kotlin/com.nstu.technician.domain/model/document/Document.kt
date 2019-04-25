f16fa5ba0e2b741154d49683321c71c3d25af597
alxminyaev <alxminyaev@gmail.com>
2019-04-25 18:54:12 +0700

    problem with kotlin-serialization

--- domain/src/main/kotlin/com.nstu.technician.domain/model/document/Document.kt
+++ domain/src/main/kotlin/com.nstu.technician.domain/model/document/Document.kt
@@ -3,13 +3,20 @@ package com.nstu.technician.domain.model.document
 import com.nstu.technician.domain.model.common.Artifact
 import com.nstu.technician.domain.model.common.OwnDateTime
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 open class Document(
-    open val oid: Long,
-    open val docType: Int,
-    open val number: String,
-    open val date: OwnDateTime,
-    open val artifact: Artifact,
+    @Transient
+    open val oid: Long = 0,
+    @Transient
+    open val docType: Int = 0,
+    @Transient
+    open val number: String = "",
+    @Transient
+    open val date: OwnDateTime = OwnDateTime(0),
+    @Transient
+    open val artifact: Artifact? = null,
+    @Transient
     open val state: Int = 0
 )
