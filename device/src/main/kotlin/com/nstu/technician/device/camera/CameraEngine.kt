cfb1e076b3bd464af4d433ca2934660075a740e4
alxminyaev <alxminyaev@gmail.com>
2019-04-29 17:10:53 +0700

    problem with cameraEngine

--- device/src/main/kotlin/com/nstu/technician/device/camera/CameraEngine.kt
+++ device/src/main/kotlin/com/nstu/technician/device/camera/CameraEngine.kt
@@ -1,75 +1,159 @@
 package com.nstu.technician.device.camera
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.*
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import android.view.Surface
 import java.util.concurrent.Semaphore
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
+
 
 class CameraEngine {
+
 
     companion object {
         const val TAG = "CameraEngine"
     }
 
-    private var cameraEngineCallBack: CameraEngineCallBack? = null
     private val cameraOpenCloseLock = Semaphore(1)
-    private var cameraDevice: CameraDevice? = null
 
     private var backgroundThread: HandlerThread? = null
     private var backgroundHandler: Handler? = null
 
-    private val stateCallBack: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+    private var mCaptureSession: CameraCaptureSession? = null
 
-        override fun onOpened(camera: CameraDevice) {
-            cameraOpenCloseLock.release()
-            cameraDevice = camera
-        }
-
-        override fun onDisconnected(camera: CameraDevice) {
-            cameraOpenCloseLock.release()
-            cameraDevice?.close()
-            cameraDevice = null
-            cameraEngineCallBack?.onDisconnected()
-        }
-
-        override fun onError(camera: CameraDevice, error: Int) {
-            onDisconnected(camera)
-        }
+    private var mTarget: Surface? by Delegates.observable<Surface?>(null) { property, oldValue, newValue ->
+        mCaptureIsReady = newValue != null && mCameraDevice != null
     }
 
-    fun onStart(callBack: CameraEngineCallBack) {
-        if (!cameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
-            throw RuntimeException("Time out waiting to lock camera opening.")
-        }
-        cameraEngineCallBack = callBack
-        startBackgroundThread()
-        val handler = backgroundHandler
-        if (handler != null) {
-            cameraEngineCallBack?.apply {
-                onSetupCamera(stateCallBack, handler)
+    private var mCameraDevice: CameraDevice? by Delegates.observable<CameraDevice?>(null) { property, oldValue, newValue ->
+        mCaptureIsReady = newValue != null && mTarget != null
+    }
+
+    private var mCaptureIsReady: Boolean by Delegates.observable(false) { property, oldValue, newValue ->
+        if (newValue) {
+            val cameraDevice = mCameraDevice
+            val surface = mTarget
+            if (cameraDevice != null && surface != null) {
+                createCaptureSession(cameraDevice, surface)
             }
         }
     }
 
-    private fun startBackgroundThread() {
-        backgroundThread = HandlerThread("CameraBackground").also { it.start() }
-        backgroundHandler = Handler(backgroundThread?.looper)
+    private val stateCallBack: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+
+        override fun onOpened(camera: CameraDevice) {
+            Log.d(TAG, "Method onOpened (stateCallBack) is called")
+            mCameraDevice = camera
+            cameraOpenCloseLock.release()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            Log.d(TAG, "Method onDisconnected (stateCallBack) is called")
+            camera.close()
+            mCameraDevice = null
+            cameraOpenCloseLock.release()
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            Log.d(TAG, "Method onError (stateCallBack) is called")
+            onDisconnected(camera)
+        }
     }
 
-    fun onCreateSession(surfaceTexture: SurfaceTexture) {
-        cameraDevice?.createCaptureSession(
-            listOf(Surface(surfaceTexture)), object : CameraCaptureSession.StateCallback() {
-                override fun onConfigureFailed(session: CameraCaptureSession) {
-                    Log.d(TAG, "onConfigureFailed is called for CameraDevice ${session.device.id}")
-                }
+    private val mCaptureSessionCallBack = object : CameraCaptureSession.CaptureCallback() {
+        override fun onCaptureProgressed(
+            session: CameraCaptureSession,
+            request: CaptureRequest,
+            partialResult: CaptureResult
+        ) {
+            Log.d(TAG, "Method onCaptureProgressed (mCaptureSessionCallBack) called")
+        }
 
+        override fun onCaptureCompleted(
+            session: CameraCaptureSession,
+            request: CaptureRequest,
+            result: TotalCaptureResult
+        ) {
+
+        }
+    }
+
+    fun onStart(context: Context) {
+        if (!cameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
+            throw RuntimeException("Time out waiting to lock camera opening.")
+        }
+
+        startBackgroundThread()
+        setupCamera(context)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setupCamera(context: Context) {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            for (cameraId in cameraManager.cameraIdList) {
+                val chars = cameraManager.getCameraCharacteristics(cameraId)
+                val facing = chars.get(CameraCharacteristics.LENS_FACING)
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_BACK) {
+                    cameraManager.openCamera(cameraId, stateCallBack, backgroundHandler)
+                } else {
+                    // TODO need handle this case
+                }
+            }
+        } catch (exception: CameraAccessException) {
+            exception.printStackTrace()
+        } catch (e: InterruptedException) {
+            throw RuntimeException("Interrupted while trying to lock camera opening.", e)
+        }
+    }
+
+    private fun startBackgroundThread() {
+        val handlerThread = HandlerThread("CameraBackground").also { it.start() }
+        backgroundHandler = Handler(handlerThread.looper)
+        backgroundThread = handlerThread
+    }
+
+    private fun stopBackgroundThread() {
+        backgroundThread?.also {
+            it.quitSafely()
+            try {
+                it.join()
+                backgroundThread = null
+                backgroundHandler = null
+            } catch (e: InterruptedException) {
+                Log.e(TAG, e.toString())
+            }
+        }
+    }
+
+    fun setSurfaceTexture(surfaceTexture: SurfaceTexture) {
+        mTarget = Surface(surfaceTexture)
+    }
+
+    private fun createCaptureSession(cameraDevice: CameraDevice, surface: Surface) {
+        val previewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        previewBuilder.addTarget(surface)
+        val previewRequest = previewBuilder.build()
+        cameraDevice.createCaptureSession(
+            listOf(surface), object : CameraCaptureSession.StateCallback() {
                 override fun onConfigured(session: CameraCaptureSession) {
                     Log.d(TAG, "onConfigured is called for CameraDevice ${session.device.id}")
+                    session.setRepeatingRequest(
+                        previewRequest,
+                        mCaptureSessionCallBack,
+                        backgroundHandler
+                    )
+                    mCaptureSession = session
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {
+                    Log.d(TAG, "onConfigureFailed is called for CameraDevice ${session.device.id}")
                 }
             },
             null
@@ -77,18 +161,17 @@ class CameraEngine {
     }
 
     fun onStop() {
-        cameraEngineCallBack = null
-        backgroundThread?.quitSafely()
+        mTarget = null
         try {
-            backgroundThread?.join()
-            backgroundThread = null
-        } catch (e: InterruptedException) {
-            Log.e(TAG, e.toString())
+            mCaptureSession?.apply {
+                abortCaptures()
+                close()
+            }
+        } catch (e: CameraAccessException) {
+            Log.d(TAG, e.toString())
+        } catch (e: java.lang.IllegalStateException) {
+            Log.d(TAG, e.toString())
         }
-    }
-
-    interface CameraEngineCallBack {
-        fun onSetupCamera(stateCallback: CameraDevice.StateCallback, handler: Handler)
-        fun onDisconnected()
+        stopBackgroundThread()
     }
 }
--- presentation/src/main/java/com/nstu/technician/feature/qr/scanner/QRCodeScannerFragment.kt
+++ presentation/src/main/java/com/nstu/technician/feature/qr/scanner/QRCodeScannerFragment.kt
@@ -1,13 +1,13 @@
 package com.nstu.technician.feature.qr.scanner
 
-import android.annotation.SuppressLint
-import android.content.Context.CAMERA_SERVICE
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
-import android.hardware.camera2.*
 import android.os.Bundle
-import android.os.Handler
-import android.view.*
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.nstu.technician.R
 import com.nstu.technician.databinding.FragmentQrcodeScannerBinding
@@ -19,81 +19,61 @@ import com.nstu.technician.feature.common.requestCameraPermission
 
 class QRCodeScannerFragment : BaseFragment() {
 
+    companion object {
+        const val TAG = "QRCodeScannerFragment"
+    }
+
     private lateinit var mBinding: FragmentQrcodeScannerBinding
 
     private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
 
         override fun onSurfaceTextureAvailable(texture: SurfaceTexture, width: Int, height: Int) {
-            cameraEngine.onCreateSession(texture)
+            Log.d(TAG, "onSurfaceTextureAvailable is available")
+            cameraEngine.setSurfaceTexture(texture)
         }
 
         override fun onSurfaceTextureSizeChanged(texture: SurfaceTexture, width: Int, height: Int) {
+            Log.d(TAG, "onSurfaceTextureSizeChanged is available")
             configureTransform(width, height)
         }
 
-        override fun onSurfaceTextureDestroyed(texture: SurfaceTexture) = true
+        override fun onSurfaceTextureDestroyed(texture: SurfaceTexture): Boolean {
+            Log.d(TAG, "onSurfaceTextureDestroyed(surfaceTextureListener) is called")
+            return true
+        }
 
-        override fun onSurfaceTextureUpdated(texture: SurfaceTexture) = Unit
-
+        override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {
+            Log.d(TAG, "onSurfaceTextureUpdated(surfaceTextureListener) is called")
+        }
     }
 
     private fun configureTransform(width: Int, height: Int) {
     }
 
-
-    private var cameraEngineCallBack: CameraEngine.CameraEngineCallBack? = null
     private val cameraEngine: CameraEngine = CameraEngine()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_qrcode_scanner, container, false)
         mBinding.texture.surfaceTextureListener = surfaceTextureListener
+
         return mBinding.root
     }
-
 
     override fun onStart() {
         super.onStart()
         if (!checkPermissionCamera(requireContext())) {
             requestCameraPermission(this@QRCodeScannerFragment)
         } else {
-            setupCallBack()
-            cameraEngineCallBack?.apply {
-                cameraEngine.onStart(this)
-            }
-        }
-    }
-
-    fun setupCallBack() {
-        cameraEngineCallBack = object : CameraEngine.CameraEngineCallBack {
-            @SuppressLint("MissingPermission")
-            override fun onSetupCamera(stateCallback: CameraDevice.StateCallback, handler: Handler) {
-                val cameraManager = requireContext().getSystemService(CAMERA_SERVICE) as CameraManager
-                try {
-                    for (cameraId in cameraManager.cameraIdList) {
-                        val chars = cameraManager.getCameraCharacteristics(cameraId)
-                        val facing = chars.get(CameraCharacteristics.LENS_FACING)
-                        if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
-                            cameraManager.openCamera(cameraId, stateCallback, handler)
-                        } else {
-                            // TODO need handle this case
-                        }
-                    }
-                } catch (exception: CameraAccessException) {
-                    exception.printStackTrace()
-                } catch (e: InterruptedException) {
-                    throw RuntimeException("Interrupted while trying to lock camera opening.", e)
-                }
-            }
-
-            override fun onDisconnected() {
-                requireActivity().finish()
+            cameraEngine.onStart(this.requireContext())
+            val texture = mBinding.texture
+            if (texture.isAvailable) {
+                cameraEngine.setSurfaceTexture(texture.surfaceTexture)
             }
         }
     }
 
     override fun onStop() {
         super.onStop()
-        cameraEngineCallBack = null
         cameraEngine.onStop()
     }
 
@@ -101,15 +81,9 @@ class QRCodeScannerFragment : BaseFragment() {
         when (requestCode) {
             PERMISSION_REQUEST_CODE_CAMERA -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    cameraEngineCallBack?.apply {
-                        cameraEngine.onStart(this)
-                    }
+                    cameraEngine.onStart(this.requireContext())
                 }
             }
         }
-    }
-
-    private fun openCamera(width: Int, height: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
